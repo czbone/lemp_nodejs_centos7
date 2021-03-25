5b6a81945c5d33b96f627863018ec0c8c0e0bd5b
naoki hirata <naoki@magic3.org>
2021-03-25 15:40:39 +0900

    不要なファイル削除。

--- .vagrant/bundler/global.sol
+++
@@ -1,1 +0,0 @@
-{"dependencies":[["vagrant-docker-compose",["= 1.5.1"]],["log4r",[">= 0"]],["concurrent-ruby",["~> 1.0"]],["i18n",[">= 0"]],["micromachine",[">= 2","< 4"]],["vagrant-vbguest",["= 0.25.0"]]],"checksum":"b85992825354cd9cae172cb3891cd4fa55d2670746fd67fb0361c280e85c9fea","vagrant_version":"2.2.14"}
